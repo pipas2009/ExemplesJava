4e4f6f03179be843a89ec6b170d828e5a2b13532
angelavivespastor <avivespastor09@gmail.com>
2026-03-08 12:54:05 +0100

    UpdateColleccionista.java

--- src/ClasseObject/Colleccionista.java
+++ src/ClasseObject/Colleccionista.java
@@ -1,4 +1,60 @@
 package ClasseObject;
 
 public class Colleccionista {
+    private Segells segells[];
+    private int numSegells;
+    private String nom;
+    private Xapes xapes[];
+    private int numXapes;
+    private int anys;
+    private String Element[];
+
+    public Colleccionista(String nom, int anys){
+        this.nom=nom; this.anys=anys;
+        this.segells=new Segells[50]; this.xapes=new Xapes[50]; this.Element= new String[5];
+    }
+
+    public void addSegell(Segells segells){
+        this.segells[this.numSegells]=segells;
+        this.numSegells ++;
+    }
+
+    public int getNumXapes(int numXapes){return numXapes;}
+    public int getNumSegells(int numSegells){return numXapes;}
+
+    public Segells SegellMésActual(){
+        int min= Integer.MAX_VALUE;
+        for (int i=0; i<segells.length; i++){
+            if(anys<segells[i].getAnys()){
+            min=i;
+            }
+        }
+        return segells[min];
+    }
+
+    public Xapes xapaMesAntiga(){
+        if(numXapes == 0) return null;
+
+        Xapes antiga = xapes[0];
+        for(int i = 1; i < numXapes; i++){
+            if(xapes[i].getAnys() < antiga.getAnys()){
+                antiga = xapes[i];
+            }
+        }
+        return antiga;
+    }
+
+    public Segells getSegellAt(int pos){return segells[pos];}
+
+    public Xapes getXapaAt(int pos){return xapes[pos];}
+
+    public void setNom(String nom){ this.nom = nom; }
+    public String getNom(){ return nom; }
+
+    public void setAnys(int anys){ this.anys = anys; }
+    public int getAnys(){ return anys; }
+
+
+
 }
+
--- src/ClasseObject/Xapes.java
+++ src/ClasseObject/Xapes.java
@@ -1,4 +1,19 @@
 package ClasseObject;
 
 public class Xapes {
+    private String nom;
+    private int anys;
+    private double preu;
+
+    public Xapes(String nom,double preu, int anys){
+        this.nom=nom; this.preu=preu; this.anys=anys;
+    }
+
+    public void setPreu(double preu) {this.preu = preu;}
+    public void setNom(String nom) {this.nom = nom;}
+    public void setAnys(int anys) {this.anys = anys;}
+
+    public String getNom() {return nom;}
+    public int getAnys() {return anys;}
+    public double getPreu() {return preu;}
 }
